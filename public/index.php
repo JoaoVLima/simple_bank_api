edf7e7b90fa27c3be4a9c5c0fc1701803874de12
JoaoVLima <celu.joaolima@gmail.com>
2024-05-25 17:43:32 -0300

    session

--- public/index.php
+++ public/index.php
@@ -3,10 +3,12 @@
 namespace Api;
 
 use Slim\Factory\AppFactory;
+use Slim\Middleware\Session;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->add(new Session(['autorefresh' => true, 'lifetime' => '1 hour',]));
 
 (require __DIR__ . '/../config/routes.php')($app);
 
