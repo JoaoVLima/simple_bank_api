b16314e918c0a361b8dfd818e44cab2a67538bd6
JoaoVLima <celu.joaolima@gmail.com>
2024-05-24 21:47:05 -0300

    Rotas

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+<?php
+namespace Api\Config;
+
+use Slim\App;
+use Api\View\CoreView;
+use Api\View\BankView;
+
+return function (App $app) {
+    $app->get('/', [CoreView::class, 'home']);
+    $app->post('/reset', [BankView::class, 'reset']);
+    $app->post('/balance', [BankView::class, 'balance']);
+    $app->post('/event', [BankView::class, 'event']);
+};
+
